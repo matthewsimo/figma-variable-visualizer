d63e39808f5697cabf1a54242e88e3ec96062ebf
Matthew Simo <matthew.a.simo@gmail.com>
2024-02-10 10:56:16 -0600

    Init shadcn, update configs, & add command component to plugin

--- packages/plugin/src/main.tsx
+++ packages/plugin/src/main.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import App from "./App";
 import "./index.css";
-import { AppProvider } from "./AppContext.tsx";
+import { AppProvider } from "./AppContext";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
